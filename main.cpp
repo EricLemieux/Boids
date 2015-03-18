5e2beb42943821f5961c7cf5e696c86a705512db
EricLemieux <ericlemieux2@gmail.com>
2015-03-17 22:32:56 -0400

    Fixed the openGL stuff

--- main.cpp
+++ main.cpp
@@ -1,144 +1,109 @@
-#include <iostream>
-
+// Link statically with GLEW
 #define GLEW_STATIC
+
+// Headers
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
-#include <GL/gl.h>
+#include <iostream>
 
-#include <GL/glu.h>
-
+// Shader sources
+const GLchar* vertexSource =
+    "#version 150 core\n"
+    "in vec2 position;"
+    "void main() {"
+    "   gl_Position = vec4(position, 0.0, 1.0);"
+    "}";
+const GLchar* fragmentSource =
+    "#version 150 core\n"
+    "out vec4 outColor;"
+    "void main() {"
+    "   outColor = vec4(1.0, 1.0, 1.0, 1.0);"
+    "}";
 using namespace std;
-
-const char* vertexShaderSrc = "#version 150\nin vec2 position;\nvoid main()\n{\ngl_Position = vec4(position, 0.0, 1.0);\n}\n";
-const char* fragmentShaderSrc = "#version 150\nout vec4 outColor;\nvoid main()\n{\noutColor = vec4(1.0,1.0,1.0,1.0);\n}\n";
-
 int main()
 {
     SDL_Window* win;
     SDL_GLContext context;
-
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
-        cout<<"Error with initing SDL.\n";
-        return 1;
+    cout<<"Error with initing SDL.\n";
+    return 1;
     }
-
     //SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
-
     /*SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);*/
-
     win = SDL_CreateWindow("Boids", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
-
     context = SDL_GL_CreateContext(win);
 
-    glewExperimental = GL_FALSE;
-    if(GLEW_OK != glewInit())
-    {
-        cout<<"Error initing glew.\n";
-        return 0;
-    }
+    SDL_GL_MakeCurrent(win, context);
+    SDL_GL_SetSwapInterval(1);
 
-    //SDL_GL_MakeCurrent(win, context);
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
 
-    //SDL_GL_SetSwapInterval(1);
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-    GLuint vertexBuffer;
-    glGenBuffers(1, &vertexBuffer);
-    printf("%u\n",vertexBuffer);
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
 
-    float verts[] =
-    {
+    GLfloat vertices[] = {
         0.0f, 0.5f,
         0.5f, -0.5f,
         -0.5f, -0.5f
     };
 
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    //Generate vertex shader
+    // Create and compile the vertex shader
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
     glCompileShader(vertexShader);
 
-    //Check the shader
-    {
-        GLint status;
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
-        if(status != GL_TRUE)
-        {
-            char buffer[512];
-            glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
-            cout<<buffer<<"\n";
-        }
-    }
-
-    //Generate fragment shader
-    GLuint fragmentShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, NULL);
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
     glCompileShader(fragmentShader);
 
-    //Check the shader
-    {
-        GLint status;
-        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
-        if(status != GL_TRUE)
-        {
-            char buffer[512];
-            glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
-            cout<<buffer<<"\n";
-        }
-    }
-
-    //Making a shader program
+    // Link the vertex and fragment shader into a shader program
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glBindFragDataLocation(shaderProgram, 0, "outColor");
     glLinkProgram(shaderProgram);
-
     glUseProgram(shaderProgram);
 
-    //Something is wrong here...
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-    GLenum e = glGetError() ;
-    if(e != GL_NO_ERROR)
+    while (true)
     {
-        printf("Error with openGL: %s\n",gluErrorString(e));
-        return 3;
-    }
 
-    GLint posAttribute = glGetAttribLocation(shaderProgram,"position");
-    glVertexAttribPointer(posAttribute,2,GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(posAttribute);
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-    //vao
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    SDL_Event windowEvent;
-
-    while(true)
-    {
-        if(SDL_PollEvent(&windowEvent))
-        {
-            if(windowEvent.type == SDL_QUIT) break;
-        }
-
+        // Draw a triangle from the 3 vertices
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        // Swap buffers
         SDL_GL_SwapWindow(win);
     }
 
-    SDL_GL_DeleteContext(context);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
-    return 0;
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+
+    glDeleteBuffers(1, &vbo);
+
+    glDeleteVertexArrays(1, &vao);
 }
